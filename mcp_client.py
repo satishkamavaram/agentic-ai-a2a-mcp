2236d79c8bdaef25cadad1c97e5ea3ab7d858329
satish kamavaram <satishkamavaram@gmail.com>
2025-08-03 20:29:30 +0200

    updated usage

--- mcp_client.py
+++ mcp_client.py
@@ -224,7 +224,7 @@ class MCPClient:
 
 async def main():
     if len(sys.argv) < 2:
-        print("Usage: python client.py <path_to_server_script>")
+        print("Usage: python mcp_client.py <path_to_server_script>")
         sys.exit(1)
 
     client = MCPClient()
